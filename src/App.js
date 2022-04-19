5f6665c37d701fc11fc68f19668dc1a900e8b822
Josh <adesanyajoshua@ymail.com>
2022-04-19 07:55:05 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
